2e6c39b140c0acba486bf0df674afbcf0feb5cb8
Hans Kindberg <hans.kindberg@gmail.com>
2018-03-01 16:25:55 +0100

    Code-backup

--- Source/Project/Web/Federation.cs
+++ Source/Project/Web/Federation.cs
@@ -11,18 +11,41 @@ namespace RegionOrebroLan.ReportingServices.Web
 		{
 			base.OnLoad(e);
 
+			this.Response.Clear();
+
 			if(this.User == null)
-				throw new InvalidOperationException("Not signed in.");
+			{
+				this.Response.Write("Not signed id.");
+				//throw new InvalidOperationException("Not signed in.");
+
+				return;
+			}
 
 			if(!(this.User.Identity is WindowsIdentity))
-				throw new InvalidOperationException("Not signed in as windows-user.");
+			{
+				this.Response.Write("<p>Not signed in as windows-user.</p>");
+				//throw new InvalidOperationException("Not signed in as windows-user.");
+
+				return;
+			}
+
+			this.Response.Write("<p>You are signed in as " + this.User.Identity.Name + "<p>");
+
+			this.Response.Write("<p>" + this.Request.Url + "</p>");
 
 			var returnUrl = this.Request.QueryString["ReturnUrl"];
 
 			if(Uri.TryCreate(returnUrl, UriKind.RelativeOrAbsolute, out var result))
-				this.Response.Redirect(result.IsAbsoluteUri ? result.AbsoluteUri : result.OriginalString, true);
+			{
+				this.Response.Redirect(result.IsAbsoluteUri ? result.AbsoluteUri : result.OriginalString, false);
 
-			this.Response.Redirect(this.Request.RawUrl.Replace("Federation.aspx", string.Empty), true);
+				return;
+			}
+
+			var uriBuilder = new UriBuilder(this.Request.Url) {Query = string.Empty};
+			uriBuilder.Path = uriBuilder.Path.Replace("Federation.aspx", string.Empty);
+
+			this.Response.Redirect(uriBuilder.Uri.ToString(), false);
 		}
 
 		#endregion
